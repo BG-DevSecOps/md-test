833af7c5d04f3519a9b196ad7e35d5f3046f2523
Karthikeyanmpk786 <129071365+Karthikeyanmpk786@users.noreply.github.com>
2023-08-25 10:38:16 +0530

    test

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -96,7 +96,7 @@ export default function Admin({ ...rest }) {
     <div className={classes.wrapper}>
       <Sidebar
         routes={routes}
-        logoText={"BG DevSecops Team"}
+        logoText={"BG DevOps Team"}
         logo={logo}
         image={image}
         handleDrawerToggle={handleDrawerToggle}
